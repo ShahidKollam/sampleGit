f74f48369203cc832f7ac7371b2d0c798a05c8c0
shahid <shahidazhari313@gmail.com>
2024-01-11 14:29:13 +0530

    ignore txt

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
         // Change the color of the heading on mouseover
         heading.style.color = '#ff4500';
     });
-
+  
     heading.addEventListener('mouseout', function() {
         // Change the color back on mouseout
         heading.style.color = '#007bff';
